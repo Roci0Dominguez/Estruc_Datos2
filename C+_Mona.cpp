8f4ff8537074c68026304ab062f430ebef24bc88
Roci0Dominguez <144483622+Roci0Dominguez@users.noreply.github.com>
2023-09-22 20:33:52 -0600

    Add files via upload

---
+++ C+_Mona.cpp
@@ -0,0 +1,4 @@
+int numero [6] = {10, 20, 30, 40, 50, 60};
+int n[]= {3, 4, 5}//Declara un array de 3 elementos
+char c[]= {'l', 'u', 'i', 's'}//Declara un array de 4elementos
+char s[]= "Mona Lisa";
